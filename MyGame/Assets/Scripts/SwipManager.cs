cf6fd12408542c048ce527b6c8fd0eec80c292c9
Administrator <admin@example.com>
2017-06-12 20:31:00 +0430

    s

--- MyGame/Assets/Scripts/SwipManager.cs
+++ MyGame/Assets/Scripts/SwipManager.cs
@@ -4,58 +4,90 @@ using UnityEngine.EventSystems;
 using UnityEngine;
 using System;
 using Lean.Touch;
+using UnityEngine.UI;
+using System.Runtime.InteropServices;
 
 public class SwipManager : MonoBehaviour
 {
 
 	public float ForceMultiplier = 10.0f;
-	public float MaxForce = 2600f;
-
-
-	Rigidbody rigidBody;
+	public float MaxForce = 3000f;
+	public Text debugText;
+	public GameObject[] players;
+	int playerMask;
+	int wallMask;
 
 	void Awake ()
-	{				
-		rigidBody = GetComponent<Rigidbody> ();
+	{					
 		LeanTouch.OnFingerSwipe += OnFingerSwipe;
+		playerMask = ~(1 << LayerMask.NameToLayer("Player"));
+		wallMask = ~(1 << LayerMask.NameToLayer("Wall"));
 	}
 	// Use this for initialization
 	void Start ()
 	{
 		
 	}
-	
+
 	// Update is called once per frame
 	void Update ()
 	{
 		
 	}
 
+	void Log(String msg) {
+		for (int i = 0; i < players.Length; i++) {			
+			msg += " player" + i + "=" + players[i].transform.position;
+		}
+		Debug.Log (msg);
+		if (debugText != null) 
+			debugText.text = msg;
+	}
+	long count = 0;
 	void OnFingerSwipe (LeanFinger finger)
-	{				
-		var ray = finger.GetStartRay(Camera.main);
-
-		RaycastHit hit;
-		if (!Physics.Raycast (ray, out hit)) {
-			return;
-		}
+	{			
+		Log ("Swipe " + count);
+		var startPoint = finger.GetStartRay().origin;
+		var endpoint = finger.GetRay().origin;
+		var direction = (endpoint - startPoint);
+		var hit = default(RaycastHit);
+		if (Physics.Raycast (new Vector3 (startPoint.x, startPoint.y, -10f), new Vector3 (0f, 0f, 1f), out hit, float.PositiveInfinity, wallMask) == true) {
+			Log ("Hit");
+			var rigidbody = hit.rigidbody;
+			var hitobject = hit.collider.gameObject;
+			// Is the rigidbody attached to this GameObject?
+			if (rigidbody != null) {
+				foreach (GameObject player in players) {
+					if (rigidbody.gameObject == player) {
+						Log ("Move" + count);
+						MoveWithVector (rigidbody, finger.SwipeScaledDelta);
+					}
+				}
+				// Add force to the rigidbody based on the swipe force
+			}
+		} else {
 			
-
-
-		// Get the rigidbody component
-		var rigidbody = hit.rigidbody;
-
-		// Is the rigidbody attached to this GameObject?
-		if (rigidbody != null && rigidbody.gameObject == gameObject) {
-			// Add force to the rigidbody based on the swipe force
-			MoveWithVector(finger.SwipeScaledDelta);
-
+			if (Physics.SphereCast (new Vector3 (startPoint.x, startPoint.y, 0f), 1f, new Vector3 (direction.normalized.x, direction.normalized.y, 0f), out hit, direction.magnitude, wallMask) == true) {
+				Log ("Hit");
+				var rigidbody = hit.rigidbody;
+				var hitobject = hit.collider.gameObject;
+				// Is the rigidbody attached to this GameObject?
+				if (rigidbody != null) {
+					foreach (GameObject player in players) {
+						if (rigidbody.gameObject == player) {
+							Log ("Move" + count);
+							MoveWithVector (rigidbody, finger.SwipeScaledDelta);
+						}
+					}
+					// Add force to the rigidbody based on the swipe force
+				}
+			}
 		}
-
+		count++;
 	}
 
 
-	void MoveWithVector (Vector2 direction)
+	void MoveWithVector (Rigidbody rigidBody, Vector2 direction)
 	{				
 		var force = direction.magnitude * ForceMultiplier;
 		if (force > MaxForce) {
@@ -66,19 +98,7 @@ public class SwipManager : MonoBehaviour
 
 	}
 
-	void OnCollisionEnter (Collision collision)
-	{		
-		if (collision.gameObject.layer == LayerMask.NameToLayer ("Wall")) {
-			var contact = collision.contacts [0];
-			var velocity = Vector3.Normalize (rigidBody.velocity + transform.position) * rigidBody.velocity.magnitude;
-			var n = Vector3.Normalize (contact.normal);
-			velocity = 2 * (Vector3.Dot (velocity, n)) * n - velocity; 
-			velocity *= -1; 
-			rigidBody.AddForce (velocity, ForceMode.Impulse);
 
-		}
-					 
-	}
 
 
 }
--- MyGame/Temp/StagingArea/gen/com/cross/myGame/R.java
+++ MyGame/Temp/StagingArea/gen/com/cross/myGame/R.java
@@ -5,7 +5,7 @@
  * should not be modified by hand.
  */
 
-package com.cross.myGame;
+package com.cross.MyGame;
 
 public final class R {
     public static final class attr {
