377c7aa9c48a98e758a2d653ea422d216e8cabc7
Mehrdad <m.h.farnoosh88@gmail.com>
2017-07-12 12:55:00 +0430

    Scene managing and level scenes added.

---
+++ MyGame/Assets/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+	public void GoToLevel (int levelNo)
+	{
+		switch (levelNo) {
+		case 0:
+			SceneManager.LoadScene ("SelectLevelScene");
+			break;
+		case 1:
+			SceneManager.LoadScene ("Level1");
+			break;
+		default:
+			SceneManager.LoadScene ("SelectLevelScene");
+			break;
+		}
+
+	}
+
+	public void GoToMain(){
+		SceneManager.LoadScene ("MainScene");
+	}
+}
