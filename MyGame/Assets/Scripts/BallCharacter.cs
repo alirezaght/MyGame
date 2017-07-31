32a709b3226fb130bcf6ba8659f02ca4bc68c216
Administrator <admin@example.com>
2017-07-31 16:59:44 +0430

    s

--- MyGame/Assets/Scripts/BallCharacter.cs
+++ MyGame/Assets/Scripts/BallCharacter.cs
@@ -12,11 +12,12 @@ public class BallCharacter : Character
 	public AudioClip BounceAudio;
 
 
-	Light GlowLight;
 
+	ParticleSystem particleSystem;
 	AudioSource audioSource;
 	new MeshRenderer renderer;
 	bool rangeIncrease = true;
+	ParticleSystem.MinMaxGradient startColor;
 
 	public override void Awake ()
 	{
@@ -25,7 +26,8 @@ public class BallCharacter : Character
 		audioSource = GetComponent<AudioSource> ();
 		EventBus.Subscribe ("HitWall", OnHitWall);
 		EventBus.Subscribe ("HitPlayer", OnHitPlayer);
-		GlowLight = GetComponentInChildren<Light> ();
+		particleSystem = GetComponentInChildren<ParticleSystem> ();
+		startColor = particleSystem.colorOverLifetime.color;
 	}
 
 	void OnHitWall (object[] info)
@@ -54,53 +56,37 @@ public class BallCharacter : Character
 
 	public override void DoSelectAnimation ()
 	{
-		if (GlowLight != null) {
-			GlowLight.intensity = 20;
-			renderer.material.SetColor ("_EmissionColor", Color.red);
+		if (particleSystem != null) {			
+			var overtime = particleSystem.colorOverLifetime;
+			overtime.color = new ParticleSystem.MinMaxGradient (Color.red);
+
 		}
 	}
 
 	public override void DoDeSelectAnimation ()
 	{
-		if (GlowLight != null) {
-			GlowLight.intensity = 5;
-			renderer.material.SetColor ("_EmissionColor", Color.blue);
-		}
+		DoStopAnimation ();
 	}
 
 	public override void DoStopAnimation ()
 	{
-		if (GlowLight != null) {
-			GlowLight.intensity = 7;
-			renderer.material.SetColor ("_EmissionColor", Color.blue);
+		if (particleSystem != null) {	
+			var overtime = particleSystem.colorOverLifetime;
+			overtime.color = startColor;
+
 		}
 	}
 
 	public override void DoMoveAnimation ()
 	{
-		if (GlowLight != null) {
-			GlowLight.intensity = 10;
-			renderer.material.SetColor ("_EmissionColor", Color.green);
+		if (particleSystem != null) {
+			var overtime = particleSystem.colorOverLifetime;
+			overtime.color = new ParticleSystem.MinMaxGradient (Color.green);
 		}
 		PlayHit ();
 	}
 
-	void Update ()
-	{
-		if (GlowLight != null) {
-			if (rangeIncrease)
-				GlowLight.range += UnityEngine.Random.Range (0.5f, 1f) * Time.deltaTime * 10;
-			else
-				GlowLight.range -= UnityEngine.Random.Range (0.01f, 0.5f) * Time.deltaTime * 7;
-			if (GlowLight.range >= 7) {
-				rangeIncrease = false;
-			}
-			if (GlowLight.range <= 3) {
-				rangeIncrease = true;
-			}
 
-		}
-	}
 
 
 }
